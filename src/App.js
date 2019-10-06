37e59f34d7970bd32126fb859744831a7e333fd6
vdewangga <virginiadewangga@gmail.com>
2019-10-06 22:48:19 +0700

    add 2.Add Todo

--- src/App.js
+++ src/App.js
@@ -13,26 +13,50 @@ import LinearGradient from 'react-native-linear-gradient';
 
 class TaskOne extends Component {
 
-  listToDo = [
-    'work',
-    'swim',
-    'study',
-    'sleep',
-    'run'
-  ]
+  constructor(){
+    super();
+    this.state = {
+      placeHoler : "Test",
+      listToDo : [
+        'work',
+        'swim',
+        'study',
+        'sleep',
+        'run'
+      ]
+    }
+  }
 
+  // listToDo = [
+    // 'work',
+    // 'swim',
+    // 'study',
+    // 'sleep',
+    // 'run'
+  // ]
+
+  addOnClick = () => {
+    this.state.listToDo.push("alpha");
+    alert(this.state.listToDo.toString())
+  }
+
+  listItem = () => {
+    return this.state.listToDo.map((el, index) => {
+      return <Text key={index} >{el}</Text>
+    })
+  }
+
+  
   render(){
     return(
       <View style={styles.container}>
         <LinearGradient colors={["#EAECC6", "#2BC0E4"]} style={styles.container}>
-          <View>
-            {this.listToDo.map((el, index)=>{
-              return(
-                <View style={[styles.borderBtm]}>
-                  <Text key={index}>{el}</Text>
-                </View>
-              )
-            })}
+          <View style={styles.inputFeald}>
+            <TextInput style={styles.inputBar}></TextInput>
+            <Button title="add" style={styles.Btn}></Button>
+          </View>
+          <View style={styles.content}>
+            <this.listItem />
           </View>
         </LinearGradient>
       </View>
@@ -51,6 +75,31 @@ const styles = StyleSheet.create(
       borderBottomWidth : 2,
       borderStyle: 'solid',
       marginBottom: 5,
+    },
+
+    content : {
+      flex : 17,
+      marginTop : 10,
+      marginLeft : 10,
+    },
+
+    inputFeald : {
+      flex : 1,
+      flexDirection : "row",
+      margin : 5,
+      alignItems : "center",
+      justifyContent : "center",
+    },
+
+    inputBtn : {
+      flex : 5,
+    },
+
+    inputBar : {
+      flex : 5,
+      borderStyle : "solid",
+      borderWidth : 1,
+      marginRight : 3,
     }
   }
 )
